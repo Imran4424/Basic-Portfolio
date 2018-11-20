cd77d730ee911b00232759ae3e54d00c0722e7f6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-20 10:33:36 +0600

    basic portfolio

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function () {
+        $("a").click(function () 
+        { 
+                //alert("I am clicked");
+
+                var selected = this;
+
+                $("a").removeClass("active");
+
+                $(selected).addClass("active");
+        });
+
+        var $a = $(".a"),
+                $b = $(".b"),
+                $c = $(".c"),
+                $d = $(".d"),
+                $home = $(".home"),
+                $about = $(".about");
+
+        $a.click(function ()
+        {
+                //alert("I am here");
+                
+                $home.fadeIn();
+                $about.fadeOut();
+        });
+
+        $b.click(function ()
+        {
+                //alert("I am here");
+
+                $about.fadeIn();
+                $home.fadeOut();
+        });
+});
